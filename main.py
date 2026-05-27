bfd45031c48fb0e11b266396cd0345a24e1027e7
赵梓健 <2592675631@qq.com>
2026-05-27 03:19:07 -0500

    add prefect flow decorator to main

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from prefect import flow
 from pre_vector import get_data, agg_extract
 from date_validation import data_validation
 from mode_recognition import *
 
-if __name__ == "__main__":
+@flow
+def main():
     df_match, df_player, df_tournament = get_data()
     pre_vector, df_agg = agg_extract(df_match, df_tournament, df_player)
     print(pre_vector["Mark Selby"])
@@ -12,4 +14,7 @@ if __name__ == "__main__":
     various_matches_set.append(df_agg)
     for df in various_matches_set:
         sequences = get_win_sequence_dataset(df)
-        analyze_top_sequences(sequences, top_n = 20)
+        analyze_top_sequences(sequences, top_n=20)
+
+if __name__ == "__main__":
+    main()
